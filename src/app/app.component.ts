0b598e190a0c56de6d6488d75ec18e5d00f6e88d
dbelcher <dillion256@gmail.com>
2023-04-21 11:11:48 -0500

    Working demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { JokeService } from './joke_service/joke_service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'joke_app';
+  Title = 'joke_app';
+  buttonText = "Show Punchline";
+  setup = "Loading...";
+  punchline = null;
+  jokeRetrieved = false;
+  showPunchline = false;
+
+  constructor (private jokeService: JokeService) {}
+
+  ngOnInit() {
+    this.jokeService.getRandomJoke().subscribe({
+      next: joke => this.handleApiResponse(joke),
+      error: error => this.handleApiError()
+    });
+  }
+
+  changeJoke() {
+    this.buttonText = this.showPunchline ? "Show Punchline" : "Hide Punchline";
+    this.showPunchline = !this.showPunchline;
+  }
+
+  handleApiResponse(joke: any) {
+    if(joke.hasOwnProperty('punchline')) {
+      this.setup = joke.setup;
+      this.punchline = joke.punchline;
+      this.jokeRetrieved = true;
+    }
+    else {
+      this.setup = "Looks like the joke API is down";
+    }
+  }
+
+  handleApiError() {
+    this.setup = "Looks like the joke API is down";
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { JokeService } from './joke_service/joke_service';
 import { AppComponent } from './app.component';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    JokeService,
+    HttpClient
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
